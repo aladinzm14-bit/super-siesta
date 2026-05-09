523e8a0cacfb6e7e768af447e1dab62f77277b11
aladinzm14-bit <aladinzm14@gmail.com>
2026-05-09 04:13:10 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      background: "#111",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Super Siesta
+    </div>
+  )
+}
